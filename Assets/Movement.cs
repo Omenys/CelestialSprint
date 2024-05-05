77772b45b09e2f29da8991d769311107bc95d870
Omenys <105769289+Omenys@users.noreply.github.com>
2024-05-05 13:42:55 -0500

    Basic Movement Functional

--- Assets/Movement.cs
+++ Assets/Movement.cs
@@ -1,18 +1,26 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
+    [SerializeField] float speed = 5f;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        // Constant movement in the +x direction
+        transform.Translate(Vector2.down * speed * Time.deltaTime);
+
+        // Input from player
+        float input = Input.GetAxis("Vertical");
+
+        // Movement in y direction
+        transform.Translate(Vector2.right * input * speed * Time.deltaTime);
+
     }
 }
