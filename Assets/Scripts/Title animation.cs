da2aef86e31ad9ad1a5ce7fb08e910916e55ddfd
osmr99 <omarsantiagomorales@gmail.com>
2024-05-08 00:35:17 -0500

    Game tittle color method and other buttons template done

--- Assets/Scripts/Title animation.cs
+++ Assets/Scripts/Title animation.cs
@@ -9,20 +9,28 @@ public class Titleanimation : MonoBehaviour
 
     //Tilting method variables
     [SerializeField] float rotZ;
-    [SerializeField] int tiltAnimTime; // 50 = 1 second;
+    [SerializeField] int tiltAnimTime; // 50 = 1 second
     int tiltAnimTick = 0;
     bool tiltAnimIn = false;
 
     //Scaling method variables
     [SerializeField] float animScale;
-    [SerializeField] float scalingAnimTime; // 50 = 1 second;
+    [SerializeField] float scalingAnimTime; // 50 = 1 second
     int scalingAnimTick = 0;
     bool scalingAnimIn = false;
+
+    //ColorChange method variables
+    Color currentColor;
+    [SerializeField] Color colorOne;
+    [SerializeField] Color colorTwo;
+    [SerializeField] float colorSpeed; // The bigger the number, the slower the speed is!!
+    float colorAnimNum;
+    public bool colorAnimBack = false;
 
     // Start is called before the first frame update
     void Start()
     {
-
+        currentColor = colorOne;
     }
 
     // Update is called once per frame
@@ -30,6 +38,8 @@ public class Titleanimation : MonoBehaviour
     {
         GameTitleText.transform.eulerAngles = new Vector3 (GameTitleText.transform.eulerAngles.x, GameTitleText.transform.eulerAngles.y, GameTitleText.transform.eulerAngles.z + rotZ);
         GameTitleText.transform.localScale = new Vector3(GameTitleText.transform.localScale.x + animScale, GameTitleText.transform.localScale.y + animScale, 0);
+        GameTitleText.color = currentColor;
+        ColorChange();
     }
 
     // Updates 50 times per second
@@ -81,6 +91,30 @@ public class Titleanimation : MonoBehaviour
                 scalingAnimTick--;
                 if(scalingAnimTick == -scalingAnimTime * 0.80)
                     scalingAnimIn = false;
+            }
+        }
+    }
+
+    void ColorChange()
+    {
+        colorAnimNum += Time.deltaTime / colorSpeed;
+
+        if(!colorAnimBack)
+        {
+            currentColor = Color.Lerp(colorOne, colorTwo, colorAnimNum);
+            if (colorAnimNum > 1)
+            {
+                colorAnimNum = 0;
+                colorAnimBack = true;
+            }
+        }
+        else
+        {
+            currentColor = Color.Lerp(colorTwo, colorOne, colorAnimNum);
+            if (colorAnimNum > 1)
+            {
+                colorAnimNum = 0;
+                colorAnimBack = false;
             }
         }
     }
