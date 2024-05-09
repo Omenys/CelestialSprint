b18a6c66ba780d639eb6b79051bfa60d62ab0259
A-Aron226 <aroeder345@gmail.com>
2024-05-09 13:34:08 -0500

    collision changes

--- Assets/Scripts/Shield Scritps/Shield.cs
+++ Assets/Scripts/Shield Scritps/Shield.cs
@@ -7,9 +7,12 @@ public class Shield : MonoBehaviour
     [SerializeField] ShieldChargeStat stats; //Accessing ShieldCharge scriptableObject
     [SerializeField] GameObject shield; //To access a shield object
 
-    private void OnTriggerEnter(Collider other)
+    private void OnTriggerEnter2D(Collider2D other)
     {
-        Destroy(shield); //Destroys shield objet from scene
-        stats.currentShieldCount++; //Adds a charge to the player's current shield count
+        if (other.gameObject.GetComponent<Movement>() != null)
+        {
+            Destroy(shield); //Destroys shield objet from scene
+            stats.currentShieldCount++; //Adds a charge to the player's current shield count
+        }
     }
 }
