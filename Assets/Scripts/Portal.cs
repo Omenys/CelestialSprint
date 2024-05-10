12cb62bb1f90e78cd1a55527a5cd90e37f984b8c
Omenys <105769289+Omenys@users.noreply.github.com>
2024-05-10 14:30:09 -0500

    Portal Outline
    - Need to merge with main to work with most updated version

---
+++ Assets/Scripts/Portal.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+
+public class Portal : MonoBehaviour
+{
+    public enum Color
+    {
+        Red = 1,
+        Blue = 2,
+        Green = 3,
+    }
+
+    int portalsEntered = 0;
+    int shieldCharges = 0;
+
+    Color color = Color.Red;
+
+    void Spawn()
+    {
+
+    }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
