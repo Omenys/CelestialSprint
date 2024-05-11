0e3dab8952b535e77f61b52ac32217d691fb00e3
A-Aron226 <aroeder345@gmail.com>
2024-05-10 20:33:40 -0500

    setting up spawning shield

--- Assets/Scripts/Portal Spawner/PortalSpawner.cs
+++ Assets/Scripts/Portal Spawner/PortalSpawner.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class PortalSpawner : MonoBehaviour
 {
     [SerializeField] GameObject portals;
+    //[SerializeField] float spawnTimer = 10f;
+    [SerializeField] float portalLife = 10f;
     float timer;
     // Start is called before the first frame update
     void Start()
@@ -27,8 +29,28 @@ public class PortalSpawner : MonoBehaviour
 
     private void portalSpawn()
     {
-        GameObject obj = Instantiate(portals, transform);
+        GameObject obj = Instantiate(portals);
+        obj.transform.position = GetPortalPosition();
+        Destroy(obj.gameObject, portalLife); //Destroys spawned object after 15 seconds (The idea of it despawning off screen)
+
+
+
+        /*GameObject obj = Instantiate(portals, transform);
         obj.transform.position = new Vector2(10, 0); //position of where the portal spawns
-        Destroy(obj.gameObject, 15); //Destroys spawned object after 15 seconds (The idea of it despawning off screen)
+        Destroy(obj.gameObject, 15);*/ //Destroys spawned object after 15 seconds (The idea of it despawning off screen)
+    }
+
+    Vector2 GetPortalPosition()
+    {
+        // Get screen boundaries
+        float screenX = Camera.main.aspect * Camera.main.orthographicSize;
+        float screenY = Camera.main.orthographicSize;
+
+        // Spawn portal within screen boundaries
+        float randomX = Random.Range(-screenX, screenX);
+        float randomY = Random.Range(-screenY, screenY);
+
+        return new Vector2(randomX, randomY);
+
     }
 }
---
+++ Assets/Scripts/Testing Scripts/Portal.cs
@@ -0,0 +1,100 @@
+using UnityEngine;
+
+public class Portal : MonoBehaviour
+{
+    [SerializeField] PortalEntered entered;
+    [SerializeField] GameObject shieldCharge;
+    GameObject obj;
+    HazardSpawner spawner;
+    Hazard asteroid;
+    PortalColor portal;
+    public Sprite portalSprite;
+    public Sprite redPortal;
+    public Sprite greenPortal;
+    public Sprite bluePortal;
+
+    public enum PortalColor
+    {
+        Red = 1,
+        Blue = 2,
+        Green = 3,
+    }
+
+    public int portalsEntered = 0; //tracks number of portals entered by player
+    int shieldCharges = 0; //tracks amount of shield charges spawned
+
+    public void Awake()
+    {
+        portalSprite = gameObject.GetComponent<SpriteRenderer>().sprite;
+        SetPortalColor();
+    }
+
+    void SetPortalColor()
+    {
+        int randomColor = UnityEngine.Random.Range(1, 4);
+        SetPortalColor((PortalColor)randomColor);
+    }
+
+
+    public void SetPortalColor(PortalColor color)
+    {
+        switch (color)
+        {
+            // If RNG is 1 spawn red portal
+            case PortalColor.Red:
+                portalSprite = redPortal;
+                break;
+            // If RNG is 2 spawn blue portal
+            case PortalColor.Blue:
+                portalSprite = bluePortal;
+                break;
+            // If RNG is 3 spawn green portal
+            case PortalColor.Green:
+                portalSprite = greenPortal;
+                break;
+            default:
+                break;
+        }
+    }
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.GetComponent<MovementWithNewInput>() != null)
+        {
+            switch (portal)
+            {
+                case (PortalColor.Red):
+                    spawner.spawnDelay = 3.5f;
+                    asteroid.maxSpeed = 300;
+                    obj = Instantiate(shieldCharge, transform);
+                    obj.transform.position = new Vector2(15, 0);
+                    break;
+                case (PortalColor.Blue):
+                    spawner.spawnDelay = 6.3f;
+                    asteroid.maxSpeed = 100;
+                    int rand = Random.Range(1, 100);
+                    if (rand < 60 && rand > 50)
+                    {
+                        obj = Instantiate(shieldCharge, transform);
+                        obj.transform.position = new Vector2(15, 0);
+                    }
+                    break;
+                case (PortalColor.Green):
+                    spawner.spawnDelay = 4.5f;
+                    asteroid.maxSpeed = 250;
+                    obj.transform.position = new Vector2(15, 0);
+                    break;
+            }
+        }
+    }
+
+
+}
